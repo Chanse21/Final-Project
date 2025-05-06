b59aa9dc0ef9a070b0b0ab6cd72d5e794c0bb0fe
Chanse21 <47192175+Chanse21@users.noreply.github.com>
2025-05-06 18:16:01 -0400

    Added Health Bar to enemy/player

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,12 +6,22 @@ public class EnemyScript : MonoBehaviour
 {
     public float speed;
     public GameObject player;
+    public float SwordDamage;
+    float health, maxHealth = 15f;
+    FloatingHealthBar healthBar;
+
 
     private float distance;
+
+    private void Awake()
+    {
+        healthBar = GetComponentInChildren<FloatingHealthBar>();
+    }
     // Start is called before the first frame update
     void Start()
     {
-
+        health = maxHealth;
+        healthBar.UpdateHealthBar(health, maxHealth);
     }
 
     // Update is called once per frame
@@ -23,12 +33,28 @@ public class EnemyScript : MonoBehaviour
         transform.position = Vector3.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
     }
 
+    public void TakeDamage(float damageAmount)
+    {
+        health -= damageAmount;
+        healthBar.UpdateHealthBar(health, maxHealth);
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+
     void OnCollisionExit(Collision collision)
     {
         Debug.Log(collision.collider.tag);
         if (collision.collider.CompareTag("Sword"))
         {
-            Destroy(gameObject);
+         TakeDamage(SwordDamage);
         }
     }
 }
